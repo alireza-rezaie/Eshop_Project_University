a0b07b299732e2f026e723326db9124fc478bf6e
unknown <alireza3356864@gmail.com>
2023-01-22 23:10:43 +0330

    AddModelAndController

---
+++ MyEshop/Models/AddEditeProductViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyEshop.Models
+{
+    public class AddEditeProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int QuantityInStock { get; set; }
+        public IFormFile Picture { get; set; }
+        public List<Category> Categories { get; set; }
+        public List<int> SelectedGroups { get; set; }
+
+    }
+}
